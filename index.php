efcd5c72e0686f3815365f8b35b938dfe28726c8
chino <sarachoesteban@gmail.com>
2024-02-09 13:38:12 -0300

    Se agrega favicon

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ You should have received a copy of the GNU General Public License along with thi
 <html lang="es">
 <head>
 	<title>Archivo Argenteam</title>
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
